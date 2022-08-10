4765b5994ebd41bafc51eb81bb1ef9b1968e79d0
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-10 19:43:25 +0800

    markup: Add front-page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'sw-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  goToPlanetList() {
+    this.router.navigateByUrl('/planet-list')
   }
-
 }
