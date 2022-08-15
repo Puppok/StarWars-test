6885772993de1e0f54ceebf35c14b0c3aa4c94b3
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-15 13:30:48 +0800

    feat: Init planet-list service

--- src/app/components/planets/planet-list/planet-list.component.ts
+++ src/app/components/planets/planet-list/planet-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {PlanetListService} from "../../../services/planet-list.service";
 
 @Component({
   selector: 'app-planet-list',
   templateUrl: './planet-list.component.html',
-  styleUrls: ['./planet-list.component.scss']
+  styleUrls: ['./planet-list.component.scss'],
+  providers: [PlanetListService]
 })
 export class PlanetListComponent implements OnInit {
 
-  constructor() { }
+  constructor(readonly planetListService: PlanetListService) { }
 
   ngOnInit(): void {
+    this.planetListService.init()
   }
 
 }
