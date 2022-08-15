7c7640adc483295768cbab172ad3684ec16a5a88
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-15 19:37:21 +0800

    feat: fetching films from api

--- src/app/components/planets/planet-card/planet-card.service.ts
+++ src/app/components/planets/planet-card/planet-card.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {catchError, EMPTY, Observable, shareReplay, switchMap, take} from "rxjs";
+import {catchError, EMPTY, Observable, of, shareReplay, switchMap, take, zip} from "rxjs";
 import {Planet} from "../../../models/planet.interface";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ApiService} from "../../../services/api.service";
+import {Film} from "../../../models/film.interface";
 
 @Injectable()
 export class PlanetCardService {
@@ -14,6 +15,12 @@ export class PlanetCardService {
       return EMPTY
     }),
     shareReplay(1)
+  )
+
+  films$: Observable<Film[]> = this.planet$.pipe(
+    switchMap(planet => planet.films.length > 0 ? zip(
+      planet.films.map(url => this.apiService.getFilm(url))
+    ) : of([]))
   )
 
   constructor(private activatedRoute: ActivatedRoute,
