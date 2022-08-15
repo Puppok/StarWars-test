31089f51bf8a1d7a4f12e2346ef7acdcb740c9e5
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-15 19:37:49 +0800

    feat: get films from api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {PlanetDTO} from "../models/planetDTO.interface";
 import {Planet} from "../models/planet.interface";
+import {Film} from "../models/film.interface";
 
 @Injectable({
   providedIn: "root"
@@ -19,5 +20,9 @@ export class ApiService {
 
   getPlanet(id: string): Observable<Planet> {
     return this.http.get<Planet>(this.url + id)
+  }
+
+  getFilm(url: string):Observable<Film> {
+    return this.http.get<Film>(url)
   }
 }
