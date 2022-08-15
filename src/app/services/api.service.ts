2c6ead5868ff7a0638c12686defa4b095139bda6
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-15 20:06:06 +0800

    feat: get residents from api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 import {PlanetDTO} from "../models/planetDTO.interface";
 import {Planet} from "../models/planet.interface";
 import {Film} from "../models/film.interface";
+import {Resident} from "../models/resident.interface";
 
 @Injectable({
   providedIn: "root"
@@ -20,6 +21,10 @@ export class ApiService {
 
   getPlanet(id: string): Observable<Planet> {
     return this.http.get<Planet>(this.url + id)
+  }
+
+  getResident(url: string): Observable<Resident> {
+    return this.http.get<Resident>(url)
   }
 
   getFilm(url: string):Observable<Film> {
