eae45b0a8752c11956c62f71978a2b288141e092
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-15 19:08:26 +0800

    feat: Get planet from api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,15 +2,22 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {PlanetDTO} from "../models/planetDTO.interface";
+import {Planet} from "../models/planet.interface";
 
 @Injectable({
   providedIn: "root"
 })
 export class ApiService {
 
+  private url = 'https://swapi.dev/api/planets/'
+
   constructor(private http: HttpClient) {}
 
-  getPlanets(url: string = 'https://swapi.dev/api/planets/'): Observable<PlanetDTO> {
-    return this.http.get<PlanetDTO>(url)
+  getPlanets(): Observable<PlanetDTO> {
+    return this.http.get<PlanetDTO>(this.url)
+  }
+
+  getPlanet(id: string): Observable<Planet> {
+    return this.http.get<Planet>(this.url + id)
   }
 }
