1e377e2c4182b6693c50e29813552525f1223a33
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-16 15:10:52 +0800

    feat: import gender-filter into planet-card

--- src/app/components/planets/planet-card/planet-card.component.ts
+++ src/app/components/planets/planet-card/planet-card.component.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
 import {PlanetCardService} from "./planet-card.service";
 import {Router} from "@angular/router";
+import {GenderFilterService} from "../../../services/gender-filter.service";
 
 @Component({
   selector: 'sw-planet-card',
   templateUrl: './planet-card.component.html',
   styleUrls: ['./planet-card.component.scss'],
-  providers: [PlanetCardService]
+  providers: [
+    PlanetCardService,
+    GenderFilterService
+  ]
 })
 export class PlanetCardComponent {
 
--- src/app/components/planets/planet-card/planet-card.module.ts
+++ src/app/components/planets/planet-card/planet-card.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {PlanetCardComponent} from "./planet-card.component";
 import {RouterModule} from "@angular/router";
+import {GenderFilterModule} from "../../ui/gender-filter/gender-filter.module";
 
 const routes = [
   {
@@ -11,10 +12,11 @@ const routes = [
 ]
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        GenderFilterModule
+    ],
   declarations: [
     PlanetCardComponent
   ]
