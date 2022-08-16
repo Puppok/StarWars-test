54646aafb723708488f9d28bd761df2c5b30e298
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-16 14:21:04 +0800

    markup: gender-filter component

---
+++ src/app/components/ui/gender-filter/gender-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sw-gender-filter',
+  templateUrl: './gender-filter.component.html',
+  styleUrls: ['./gender-filter.component.scss']
+})
+export class GenderFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
