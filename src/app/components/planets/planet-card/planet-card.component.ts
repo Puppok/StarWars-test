d0523a4b425ba952cf9d8dc2790b4eea1ab70568
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-08-16 15:28:42 +0800

    feat: filter residents in planet-card

--- src/app/components/planets/planet-card/planet-card.component.ts
+++ src/app/components/planets/planet-card/planet-card.component.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {PlanetCardService} from "./planet-card.service";
 import {Router} from "@angular/router";
 import {GenderFilterService} from "../../../services/gender-filter.service";
+import {Resident} from "../../../models/resident.interface";
+import {GenderFilterOptions} from "../../../models/gender-filter-options";
+import {combineLatest, map} from "rxjs";
 
 @Component({
   selector: 'sw-planet-card',
@@ -12,12 +15,34 @@ import {GenderFilterService} from "../../../services/gender-filter.service";
     GenderFilterService
   ]
 })
-export class PlanetCardComponent {
+export class PlanetCardComponent implements OnDestroy{
 
-  constructor(readonly planetCardService: PlanetCardService,
-              private router: Router) { }
+  residents$ = combineLatest([this.planetCardService.residents$, this.genderFilterService.option$]).pipe(
+    map(([residents, genderFilterOptions]) => PlanetCardComponent.filteredResidents(residents, genderFilterOptions))
+  )
+
+  constructor(private router: Router,
+              readonly planetCardService: PlanetCardService,
+              readonly genderFilterService: GenderFilterService) { }
 
   toPlanetList() {
     this.router.navigateByUrl('/planet-list')
+  }
+
+  private static filteredResidents(residents: Resident[], genderFilterOption: GenderFilterOptions): Resident[] {
+    let filteredResidents: Resident[] = [...residents]
+    switch (genderFilterOption) {
+      case GenderFilterOptions.MALE:
+        return filteredResidents.filter(resident => resident.gender === 'male')
+      case GenderFilterOptions.FEMALE:
+        return filteredResidents.filter(resident => resident.gender === 'female')
+      case GenderFilterOptions.UNKNOWN:
+        return filteredResidents.filter(resident => resident.gender === 'n/a')
+    }
+    return filteredResidents
+  }
+
+  ngOnDestroy() {
+    this.genderFilterService.clearOption()
   }
 }
